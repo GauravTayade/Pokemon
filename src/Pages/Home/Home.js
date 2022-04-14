b7274ff3eca98cb654f8eb96ad7b8edb23ddce33
Gaurav Tayade <taydegauravb@gmail.com>
2022-04-13 22:28:44 -0400

    UI changes and api changes has been made

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,7 @@ import {
     Grid,
     ListItem,
     List,
-    ListItemAvatar, Avatar, ListItemText
+    ListItemAvatar, Avatar, ListItemText, Typography
 } from "@mui/material";
 
 import {ChildFriendly} from "@mui/icons-material";
@@ -52,12 +52,11 @@ const Home = (props)=> {
 
                     Promise.all(
                         response.data.results.map(async(pokemon)=>{
-                            console.log('main request',response.data.results[1]);
                             await axios.get(pokemon.url)
                                 .then(response=>{
                                     pokemonArray.push({
                                         name:pokemon.name,
-                                        url: response.data.species.url,
+                                        url: {character:pokemon.url,species:response.data.species.url},
                                         image: response.data.sprites.other['official-artwork'].front_default
                                     })
 
@@ -79,11 +78,12 @@ const Home = (props)=> {
 
                     Promise.all(
                         response.data.results.map(async(pokemon)=>{
+
                             await axios.get(pokemon.url)
                                 .then(response=>{
                                     pokemonArray.push({
                                         name:pokemon.name,
-                                        url: response.data.species.url,
+                                        url: {character:pokemon.url,species:response.data.species.url},
                                         image: response.data.sprites.other['official-artwork'].front_default
                                     })
 
@@ -98,22 +98,37 @@ const Home = (props)=> {
     }
 
     const getPokemonDetails =async(url) =>{
-        await axios.get(url).then(async (response)=>{
-            console.log(response.data);
-           await setObjPokemon({
-                name:response.data.name,
-                shape:response.data.shape,
-                color: response.data.color.name,
-                habitat:response.data.habitat.name,
-                basic_form: response.data.evolves_from_species? response.data.evolves_from_species.name:'',
-                growth_rate: response.data.growth_rate.name,
-                baby:response.data.is_baby ? 'Yes' : 'No',
-                mythical: response.data.is_mythical ? 'Yes' : 'No',
-                legendary: response.data.is_legendary ? 'Yes' : 'No'
-            })
 
+        const pokemonObj ={}
+        Promise.all([
+        await axios.get(url.character).then(async(response)=>{
+
+                pokemonObj.pokemonImage=response.data.sprites.other['official-artwork'].front_default;
+                pokemonObj.abilities=response.data.abilities;
+                pokemonObj.moves=response.data.moves;
+                pokemonObj.height=response.data.height;
+
+        })
+        ,
+        await axios.get(url.species).then(async (response)=>{
+
+                pokemonObj.name=response.data.name;
+                pokemonObj.shape=response.data.shape;
+                pokemonObj.color= response.data.color.name;
+                pokemonObj.habitat=response.data.habitat.name;
+                pokemonObj.basic_form= response.data.evolves_from_species? response.data.evolves_from_species.name:'';
+                pokemonObj.growth_rate= response.data.growth_rate.name;
+                pokemonObj.baby=response.data.is_baby ? 'Yes' : 'No';
+                pokemonObj.mythical= response.data.is_mythical ? 'Yes' : 'No';
+                pokemonObj.legendary= response.data.is_legendary ? 'Yes' : 'No';
+
+        })
+        ]).then(()=>{
+        console.log(pokemonObj)
+            setObjPokemon(pokemonObj);
             setIsDrawer(true);
         })
+
     }
 
     useEffect(async()=>{
@@ -156,51 +171,68 @@ const Home = (props)=> {
             <Drawer anchor='bottom' open={isDrawer} onClose={closeDrawer}>
                 {objPokemon ?
                     <Grid container sx={{display:"flex",justifyContent:"center"}}>
-                        <Grid container >
-                            <Grid item xs={4} sx={{display:"flex",justifyContent:"center"}}>
-                                <Card sx={{justifyContent:'center', display:'flex',width:'100%',flexWrap:'wrap'}}>
-                                    <CardContent>
-                                        <h3>{objPokemon.name}</h3>
-                                    </CardContent>
-                                    <CardMedia
-                                        component="img"
-                                        sx={{width:'350px',}}
-                                        src="https://i.pinimg.com/564x/e4/b1/78/e4b1789696e9c9353533ba4a7cb968bd.jpg"/>
-                                </Card>
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <Typography variant="h3" component="h3" sx={{display:'flex',justifyContent:'center',padding:'0.7rem',backgroundColor:'#F0BB62'}}>
+                                    {objPokemon.name.toUpperCase()}
+                                </Typography>
                             </Grid>
-                            <Grid item xs={4}>
-                                <List sx={{width:'100%', bgColor:'background.paper'}}>
-                                    <ListItem>
-                                        <ListItemText primary="BABY" secondary={objPokemon.baby}></ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText primary="BASIC FORM" secondary={objPokemon.basic_form}></ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText primary="GROWTH RATE" secondary={objPokemon.growth_rate}></ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText primary="COLOR" secondary={objPokemon.color}></ListItemText>
-                                    </ListItem>
-                                </List>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <List>
-                                    <ListItem>
-                                        <ListItemText primary="HABITAT" secondary={objPokemon.habitat}></ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText primary="SHAPE" secondary={objPokemon.shape.name}></ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText primary="LEGENDARY" secondary={objPokemon.legendary}></ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText primary="MYTHICAL" secondary={objPokemon.mythical}></ListItemText>
-                                    </ListItem>
-                                </List>
+                            <Grid item xs={12}>
+                                <Grid container>
+                                    <Grid item xs={3} sx={{display:"flex",justifyContent:"center"}}>
+                                        <Card sx={{justifyContent:'center', display:'flex',width:'100%',flexWrap:'wrap'}}>
+                                            <CardMedia
+                                                component="img"
+                                                sx={{width:'350px',}}
+                                                src={objPokemon.pokemonImage}/>
+                                        </Card>
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <Grid container>
+                                            <Grid item xs={6}>
+                                                <List sx={{width:'100%', bgColor:'background.paper'}}>
+                                                    <ListItem>
+                                                        <ListItemText primary="BABY" secondary={objPokemon.baby}></ListItemText>
+                                                    </ListItem>
+                                                    <ListItem>
+                                                        <ListItemText primary="BASIC FORM" secondary={objPokemon.basic_form}></ListItemText>
+                                                    </ListItem>
+                                                    <ListItem>
+                                                        <ListItemText primary="GROWTH RATE" secondary={objPokemon.growth_rate}></ListItemText>
+                                                    </ListItem>
+                                                    <ListItem>
+                                                        <ListItemText primary="COLOR" secondary={objPokemon.color}></ListItemText>
+                                                    </ListItem>
+                                                </List>
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <List>
+                                                    <ListItem>
+                                                        <ListItemText primary="HABITAT" secondary={objPokemon.habitat}></ListItemText>
+                                                    </ListItem>
+                                                    <ListItem>
+                                                        <ListItemText primary="SHAPE" secondary={objPokemon.shape.name}></ListItemText>
+                                                    </ListItem>
+                                                    <ListItem>
+                                                        <ListItemText primary="LEGENDARY" secondary={objPokemon.legendary}></ListItemText>
+                                                    </ListItem>
+                                                    <ListItem>
+                                                        <ListItemText primary="MYTHICAL" secondary={objPokemon.mythical}></ListItemText>
+                                                    </ListItem>
+                                                </List>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={3}>
+
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <AbilitiesList abilities={objPokemon.abilities}/>
+                                    </Grid>
+                                </Grid>
                             </Grid>
                         </Grid>
+
                     </Grid>
 
                     :
